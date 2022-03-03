da0c61d4ee59ee9a369b18089ced7ddee7b8c00a
BhushanKhandekar <99717082+BhushanKhandekar@users.noreply.github.com>
2022-03-03 10:37:23 +0530

    Add files via upload

---
+++ distance_conversion_km_to_m.py
@@ -0,0 +1,5 @@
+# Conversion distance from kilometre to meter
+km = float(input("Enter the distance in Kilometre : "))
+m = km * 1000
+print("-------------Conversion---------------")
+print(km,"Kilometre =",m,"meter")
---
+++ temperature_conversion_celcius_to_fahreneit.py
@@ -0,0 +1,5 @@
+# Conversion of Temperature from Celsius to Fahrenheit
+c = float(input("Enter the Temperature in Celsius : "))
+f = ( c * 1.8 ) + 32
+print("-------------Conversion---------------")
+print(c,"degree celsius = ",f,"degree fahrenheit")
